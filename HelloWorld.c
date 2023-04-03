ff7e4d76f0abce2acdcf28133d02d39cfe67f8f4
Christine Oyalo <christineoyalo@Christines-MacBook-Pro.local>
2023-04-03 01:27:04 -0400

    Change greeting to 'Very Crool'

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-    printf("Hello World!\n");
+    printf("Hello Very Crool World!");
 
     return(EXIT_SUCCESS);
 }
